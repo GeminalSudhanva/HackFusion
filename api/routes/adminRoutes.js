061b6464a6cf3cb38ab81bb0def7a9b4711b0269
Sudhanva Ballary <ukgaming329@gmail.com>
2026-04-02 23:32:46 +0530

    Refactor: Definitive backend isolation in server/ to resolve Vercel routing conflicts

--- api/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllTeamsWithRegistrations } = require('../controllers/adminController');
-const { protect, admin } = require('../middleware/authMiddleware');
-
-router.get('/teams', protect, admin, getAllTeamsWithRegistrations);
-
-module.exports = router;
--- api/routes/registrationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerTeam, getMyRegistration } = require('../controllers/registrationController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/register', protect, registerTeam);
-router.get('/my-registration', protect, getMyRegistration);
-
-module.exports = router;
