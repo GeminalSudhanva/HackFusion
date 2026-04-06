cc664566b09967461aa6943e66a2e1fad0bee78f
Sudhanva Ballary <ukgaming329@gmail.com>
2026-04-06 20:32:37 +0530

    Fix: Restored missing express and router in adminRoutes.js

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const router = express.Router();
 const { getAllTeamsWithRegistrations, scanFoodTeam } = require('../controllers/adminController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
